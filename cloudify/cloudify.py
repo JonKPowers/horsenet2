cf1563c678fa3952af0ca354bf88a6323cd87d56
Jon Powers <Jon.K.Powers+git@gmail.com>
2020-02-07 07:54:48 -0500

    Morning commit. Added unzip and upload function to cloudify.

--- cloudify/cloudify.py
+++ cloudify/cloudify.py
@@ -1,11 +1,15 @@
 import os
-import zipfile
+from zipfile import ZipFile
 import re
 from pathlib import Path
 import hashlib
+import logging
+import boto3
+from typing import List
+
 
 INBOUND_DIR = '/data/python/horsenet_2/inbound_horse_data'
-STORAGE_BUCKET = 'horsenet-data-files'
+BUCKET = 'horsenet-data-files'
 ZIP_FOLDER = 'zip-files'
 EXTRACTED_FOLDER = 'extracted-files'
 
@@ -17,6 +21,7 @@ def process_inbound_dir() -> None:
             file.unlink()
 
     # Next unzip the remaining files and upload them along with the original zip
+    s3 = boto3.client('s3')
     file_list: List[Path] = get_file_list(INBOUND_DIR)
     with tempfile.TemporaryDirectory() as temp_dir:
         for file in file_list:
@@ -49,5 +54,23 @@ def get_file_list(dir: str, dupes_only=False) -> List[Path]:
     if dupes_only:
         file_list = [file for file in file_list if re.search(r' ?\(\d\)', file.stem)]
     return file_list
+
+def unzip_add_year(zip_file: Path, dir: str, s3) -> None:
+    pass
+
+def unzip(zip_file: Path, dir: str, s3) -> None:
+    zipf = ZipFile(zip_file)
+    zipped_files = zipf.namelist()
+    for file in zipped_files:
+        path = zipf.extract(file, path=dir)
+        try:
+            success = s3.upload_file(path, BUCKET, EXTRACTED_FOLDER + '/' + file)
+        except:
+            logging.error(f'Upload failed: {file}')
+
+    try:
+        success = s3.upload_file(str(zip_file), BUCKET, ZIP_FOLDER + '/' + zip_file.name)
+    except:
+        logging.error(f'Upload failed: {zip_file.name}')
 
 
